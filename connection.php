4ed07d9c62f4628dad435b090d1d5f16c3988d43
Dam <damvtngcc2002@fpt.edu.vn>
2022-10-20 10:20:53 +0700

    git add .

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+// $conn = mysqli_connect('localhost:81', 'root', '', 'asm2_200109')
+// or die("Can not connect database".mysqli_connect_error());
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "asm2_200109";
+
+$conn = pg_connect("postgres://kpxccmyoqzkujz:cdc818481992e58d6b88fa62b490c7c92257f6fde993c7f7670362fb1b51fa21@ec2-3-93-206-109.compute-1.amazonaws.com:5432/d2uuuugm90ocgr");
+
+// $connect = pg_connect("postgres://kpxccmyoqzkujz:cdc818481992e58d6b88fa62b490c7c92257f6fde993c7f7670362fb1b51fa21@ec2-3-93-206-109.compute-1.amazonaws.com:5432/d2uuuugm90ocgr");
+
+// if(!$connect){
+//     die("Connect failed");
+// }
+?>
